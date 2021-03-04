53b683bdfd112cbd3ffcb6694ebdc6cd19f51d50
Joseph M. Myers <joecaricature@gmail.com>
2021-03-03 20:05:02 -0500

    added menu options for howework prompts

--- Program.cs
+++ Program.cs
@@ -11,22 +11,24 @@ namespace RhythmsGonnaGetYou
             var db = new RhythmContext();
             var bands = db.Bands;
 
-            var band = new Band()
-            {
-                Name = "The Cure",
-                CountryOfOrigin = "UK",
-                NumberOfMembers = 5,
-                Website = "https://thecure.com",
-                Genre = "Post-Punk",
-                IsSigned = true,
-                ContactName = "Robert Smith",
-                ContactPhoneNumber = "+114230984985702"
-            };
-
-            bands.Add(band);
-            db.SaveChanges();
-
-            Console.WriteLine($"There are {bands.Count()} bands!");
+            MenuGreeting("Main Menu. Please make a selection:");
+            Console.WriteLine("1) Add a Band to the Database");
+            Console.WriteLine("2) View all Bands in the Database");
+            Console.WriteLine("3) Add an Album for a Band in the Database");
+            Console.WriteLine("4) Add a Song to an Album in the Database");
+            Console.WriteLine("5) Cut a Band in the Database FROM the Label");
+            Console.WriteLine("6) Sign a Band in the Database TO the Label");
+            Console.WriteLine("7) View all of a specific Band's Albums");
+            Console.WriteLine("8) View all Albums in the Database (ordered by release date)");
+            Console.WriteLine("9) View all Bands that are signed to the Label");
+            Console.WriteLine("10) View all Bands that are NOT signed to the Label");
+            Console.WriteLine("Type (Q)uit to Exit the program.");
+        }
+        static void MenuGreeting(string message)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Welcome to the {message}");
+            Console.WriteLine(("").PadRight(55, '-'));
         }
     }
 }
