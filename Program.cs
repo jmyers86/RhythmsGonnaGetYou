f0ab72aefcf91cbe9f930175c73e84638f2dc16d
Joseph M. Myers <joecaricature@gmail.com>
2021-03-03 19:06:30 -0500

    Set up Entities and Context

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Linq;
+using RhythmsGonnaGetYou.Models;
 
 namespace RhythmsGonnaGetYou
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var db = new RhythmContext();
+            var bands = db.Bands;
+
+            var band = new Band()
+            {
+                Name = "The Cure",
+                CountryOfOrigin = "UK",
+                NumberOfMembers = 5,
+                Website = "https://thecure.com",
+                Genre = "Post-Punk",
+                IsSigned = true,
+                ContactName = "Robert Smith",
+                ContactPhoneNumber = "+114230984985702"
+            };
+
+            bands.Add(band);
+            db.SaveChanges();
+
+            Console.WriteLine($"There are {bands.Count()} bands!");
+        }
     }
-  }
 }
---
+++ Song.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace RhythmsGonnaGetYou.Models
+{
+    class Song
+    {
+        public int Id { get; set; }
+        public Album Album { get; set; }
+        public string Title { get; set; }
+        public int TrackNumber { get; set; }
+        public int Duration { get; set; }
+    }
+}
